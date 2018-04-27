f5ac8116ced546b83ce224b2a7da256bdaf77609
Mirek Dworski <miroslaw.dworski@gmail.com>
2018-04-27 09:19:39 +0200

    Test sciezki

--- Kadry.java
+++ Kadry.java
@@ -1,29 +1,49 @@
 package Kadry;
 
-public class Kadry extends Pracownik {
+public class Kadry {
 
-	private Pracownik[] pracownicy_; 
+	private Pracownik[] pracownicy_;
 	private int zatrudnienie = 0;
 	
-	public Kadry(String i, String n, double p, char s, int d) {	
-		super(i,n,p,s,d);
-		pracownicy_[] = new Pracownik (i,n,p,s,d);
+	public Kadry() {	
+		this.pracownicy_ = new Pracownik[100]; 
 		}
 	
-	public void dodajPracownika(String i, String n, double p, char s, int d) {
-		if (this.zatrudnienie<=99) {
-			this.pracownicy[zatrudnienie] = new Pracownik (i,n,p,s,d);
+	public void dodajPracownika(Pracownik p) {
+		if (this.zatrudnienie<=100) {
+			this.pracownicy_[zatrudnienie] = p;
 			(this.zatrudnienie)++;
 		}else {
 				System.out.println("Niestety nie mozemy cie juz zatrudnic. Idz do PUPu");
 		}
 	}
 
+	public void dodajPracownikainteraktywnie() {
+		
+		
+	}
+	public void importujZplikuTekstowego() {
+		
+		
+	}
+	
+	public double sredniZarobek() {
+		return 1.0;
+	}
+	
+	public double sredniZarobek(int d) {
+		return 1.0;
+	}
+	
 	public static void main(String[] args) {
 		
-		Pracownik pracownik1 = new Pracownik("Jan", "Nowak", 23.00, 'M', 45);
-		pracownik1.czyPracujeWDziale(45);
-		
+		Kadry k = new Kadry();
+		Pracownik p1 = new Pracownik("Jan", "Nowak", 2300.00, 'M', 45);
+		k.dodajPracownika(p1);
+		System.out.println(p1.toString());
+		/*for(Pracownik x:pracownicy_) {
+			System.out.println(x);
+		}*/
 
 		
 	}
